006e91aa93a510232fe0a6e6ea1aef0297582e46
1sh1d4 <isd97cd8adb@icloud.com>
2025-09-01 17:12:13 +0900

    Appコンポーネントのスタイルを整理し、ノートの表示を改善

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 const App = () => {
-  const [title, setTitle] = useState([]);
-  const [content, setContent] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const [notes, setNotes] = useState([]);
+  const [selectedIndex, setSelectedIndex] = useState(null)
 
   //ページ読み込み時にlocalStrageから復元
   useEffect (() => {
@@ -16,46 +17,56 @@ const App = () => {
 
   //notesが変わるたびにlocalStrageに保存
   useEffect (() => {
-    localStorage.setItem("notes", JSON.stringify(notes));
-  }, [notes]);
+    localStorage.setItem("notes", JSON.stringify(notes))
+  },[notes]);
 
   //新しいノートを追加
   const addNote = () => {
-    if (title.trim() === "" && content.trim() === "") return;
-    const newNote = {title, content};
+    const newNote = { title, content };
     setNotes([...notes, newNote]);
-    setTitle("");
-    setContent("");
+    setSelectedIndex(notes.length);
+    setTitle("")
+    setContent("")
   }
 
   return(
     <div>
-      <h1>ノートアプリ</h1>
-
+      <p>ノートアプリ</p>
       <div>
-        <input
-          type="text"
-          placeholder="タイトルを入力"
-          value={title}
-          onChange={(e)=> setTitle(e.target.value)}
-        />
-        <textarea
-          placeholder="テキストを入力"
-          value={content}
-          onChange={(e)=> setContent(e.target.value)}
-        />
-        <button onClick={addNote}>保存</button>
+        <div>
+          <input
+            type="text"
+            placeholder="タイトルを入力"
+            value={title}
+            onChange={(e)=> setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <textarea
+            placeholder="テキストを入力"
+            value={content}
+            onChange={(e)=> setContent(e.target.value)}
+          />
+        </div>
+        <div>
+          <button onClick={addNote}>保存</button>
+        </div>
       </div>
-
-      <h2>保存されたノート</h2>
-      <ul>
-        {notes.map((note, index) => (
-          <li key={index}>
-            <strong>{note.title}</strong>
-            <p>{note.content}</p>
-          </li>
-        ))}
-      </ul>
+      <div>
+        <p>保存されたノート一覧</p>
+        <ul>
+          {notes.map((note, index) => (
+            <li key={index}>
+              <p>{note.title || " （無題） "}</p>
+              <p>
+                {note.content.length>100 
+                  ? note.content.substring(0, 100) + "..."
+                  : note.content}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
