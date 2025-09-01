1c0316e7b0573451e719bdc993c0fd94e1bf9666
1sh1d4 <isd97cd8adb@icloud.com>
2025-09-01 19:38:10 +0900

    削除機能追加

--- src/App.jsx
+++ src/App.jsx
@@ -35,49 +35,63 @@ const App = () => {
   //ノート選択
   const selectedNote = selectedIndex !== null ? notes[selectedIndex] : null;
 
+  //ノート削除
+  const deleteNote = (index) => {
+    const updatedNotes = notes.filter((note, i) => i !== index);
+    setNotes(updatedNotes)
+  }
+
   return(
-    <div>
-      <p>ノートアプリ</p>
-      <div>
-        <button onClick={addNote}>追加</button>
+    <div className="app">
+      <div className="sidebar">
+        <button
+          className="add-button"
+          onClick={addNote}>ノートを追加する
+        </button>
+        <ul className="note-list">
+          {notes.map((note, index) =>
+            <li
+              key={index}
+              className={`note-item ${selectedIndex === index ? "active" : ""}`}
+              onClick={() => setSelectedIndex(index)}
+            >
+            <span>
+              {note.title || "タイトル無し"}
+            </span>
+            <button
+              className="delete-button"
+              onClick={() => deleteNote(index)}
+            >削除
+            </button>  
+            </li>
+          )}
+        </ul>
       </div>
 
-      {selectedNote && (
-        <div>
-          <div>
-            <input
-              type="text"
-              placeholder="タイトルを入力"
-              value={selectedNote.title}
-              onChange={(e) => updateNote(selectedIndex, "title", e.target.value)}
-            />
-            <div>
+      <div className="editor-preview">
+        {selectedNote && (
+          <>
+            <div className="editor">
+              <input
+                className="title-input"
+                type="text"
+                placeholder="タイトルを入力"
+                value={selectedNote.title}
+                onChange={(e) => updateNote(selectedIndex, "title", e.target.value)}
+              />
               <textarea
+                className="content-textarea"
                 placeholder="入力してください"
                 value={selectedNote.content}
                 onChange={(e) => updateNote(selectedIndex, "content", e.target.value)}
               />
             </div>
-          </div>
-          <div>
-            <h1>プレビュー</h1>
-            <p>{selectedNote.title || "タイトル未入力です"}</p>
-            <p>{selectedNote.content || "内容未入力"}</p>
-          </div>
-        </div>
-      )}
-
-      <div>
-        <ul>
-          {notes.map((note, index) =>
-            <li
-            key={index}
-            onClick={() => setSelectedIndex(index)}
-            >
-              {note.title || "タイトル無し"}
-            </li>
-          )}
-        </ul>
+            <div className="preview">
+              <h1>{selectedNote.title}</h1>
+              <p>{selectedNote.content}</p>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
